af937f32466a59c975ae2f6341c486c93a6cf4d7
Javier Olan <javikin10@gmail.com>
2014-01-30 20:27:36 -0600

    add ponny for send mails

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@
 require 'sinatra'
 require 'sinatra/flash'
 require 'sinatra/redirect_with_flash'
+require 'pony'
 
 enable :sessions
 
@@ -19,5 +20,26 @@ end
 
 get "/" do  
   @title = "Inicio"
+  erb :"index"
+end
+
+
+post "/contacto" do
+  Pony.mail({
+  :from => params[:email],
+    :to => 'contacto@yellowme.mx', #mail que recibira el correo    
+    :subject => params[:email] + " " + params[:name] + " esta interesado",
+    :body => params[:message],
+    :via => :smtp,
+    :via_options => {
+     :address              => 'smtp.gmail.com',
+     :port                 => '587',
+     :enable_starttls_auto => true,
+     :user_name            => 'humanitymx@gmail.com', #mail para usar como mailer
+     :password             => 'Human123', #password del mismo mail
+     :authentication       => :plain, 
+     :domain               => "localhost.localdomain" 
+    }
+  })
   erb :"index"
 end
